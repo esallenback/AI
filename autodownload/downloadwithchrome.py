886f94f6c175118243a289cf97c954fd097bed08
wecreate1 <jimmy@salleback.net>
2018-07-13 16:09:25 -0500

    updated downloadwithchrome.py for periodic download sessions

--- autodownload/downloadwithchrome.py
+++ autodownload/downloadwithchrome.py
@@ -7,12 +7,23 @@ chromeOptions.add_experimental_option("prefs",prefs)
 driver = webdriver.Chrome("chromedriver.exe",chrome_options=chromeOptions)  # Optional argument, if not specified will search path.
 driver.get('https://www.opensubtitles.org/en/search/subs');
 time.sleep(5) # Let the user actually see something!
+startatf=open('downloadat.txt','+r')
+startat=startatf.read()
+startatf.close()
+if startat == '':
+    startat=0
+else:
+    startat=int(startat)
 file=open('links.txt','r')
 links=file.read()
 links=links.split('\n')
-for x in links:
+for x in links[0+startat:(startat+10)]:
     driver.execute_script('window.location.replace("https://www.opensubtitles.org/en/subtitleserve/sub/{}")'.format(x))
-    time.sleep(30)
+    time.sleep(3)
     print(x)
 time.sleep(10) # Let the user actually see something!
 driver.quit()
+file.close()
+file=open('downloadat.txt','+w')
+file.write(str(startat+10))
+file.close()
